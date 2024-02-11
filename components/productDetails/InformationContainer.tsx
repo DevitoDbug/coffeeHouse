b932e47d71961e0e573b7f73b837aced4af229af
devitoDbug <davidOchiengy@gmail.com>
2024-02-11 08:39:41 +0300

    refactor: Refactor code to improve performance and readability

---
+++ components/productDetails/InformationContainer.tsx
@@ -0,0 +1,150 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTSIZE,
+  textlight_regular,
+  textlight_semibold,
+  textlightfaint_medium,
+} from "../../assets/images/constants";
+
+export const InformationContainer = (): JSX.Element => {
+  return (
+    <View style={styles.bottomContainer}>
+      <Text
+        style={{
+          ...textlight_semibold,
+          fontSize: 14,
+          marginTop: 19,
+          marginBottom: 15,
+          color: COLORS.secondaryLightGreyHex,
+        }}
+      >
+        Description
+      </Text>
+      <Text
+        style={{
+          ...textlight_regular,
+          fontSize: FONTSIZE.size_12,
+          marginBottom: 8,
+        }}
+      >
+        Arabica beans are by far the most popular type of coffee beans, making
+        up about 60% of the world’s coffee. These tasty beans originated many
+        centuries ago in the highlands of Ethiopia, and may even be the first
+        coffee beans ever consumed!
+      </Text>
+      <Text
+        style={{
+          ...textlight_semibold,
+          fontSize: 14,
+          marginTop: 8,
+          marginBottom: 12,
+          color: COLORS.secondaryLightGreyHex,
+        }}
+      >
+        Size
+      </Text>
+      <View style={styles.sizeContainer}>
+        <TouchableOpacity style={[styles.size, styles.selectedSize]}>
+          <Text
+            style={[
+              textlightfaint_medium,
+              styles.selectedSizeText,
+              { fontSize: 12 },
+            ]}
+          >
+            250gm
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.size}>
+          <Text style={{ ...textlightfaint_medium, fontSize: 12 }}>500gm</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.size}>
+          <Text style={{ ...textlightfaint_medium, fontSize: 12 }}>1000gm</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.priceArea}>
+        <View style={{ display: "flex", gap: 3 }}>
+          <Text
+            style={{
+              ...textlightfaint_medium,
+              fontSize: 12,
+              textAlign: "center",
+            }}
+          >
+            Price
+          </Text>
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            <Text
+              style={{
+                ...textlight_semibold,
+                fontSize: 20,
+                color: COLORS.primaryOrangeHex,
+              }}
+            >
+              $
+            </Text>
+            <Text style={{ ...textlight_semibold, fontSize: 20 }}> 25.00</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: COLORS.primaryOrangeHex,
+            width: 240,
+            height: 60,
+            borderRadius: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              ...textlight_semibold,
+              fontSize: 16,
+            }}
+          >
+            Add to Cart
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    paddingHorizontal: 18,
+  },
+  sizeContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 28,
+  },
+  size: {
+    width: 100,
+    height: 40,
+    borderRadius: BORDERRADIUS.radius_10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primaryDarkGreyHex,
+  },
+  selectedSize: {
+    borderWidth: 2,
+    borderColor: COLORS.primaryOrangeHex,
+  },
+  selectedSizeText: {
+    color: COLORS.primaryOrangeHex,
+  },
+  priceArea: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
