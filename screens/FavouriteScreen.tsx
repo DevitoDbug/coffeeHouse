0e3e96a867b3caca79e76fc4ba8602c96f3095d8
devitoDbug <davidOchiengy@gmail.com>
2024-02-16 21:48:36 +0300

    feat: Implement favorite screen components

--- screens/FavouriteScreen.tsx
+++ screens/FavouriteScreen.tsx
@@ -1,50 +1,73 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { FlatList, ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  BORDERRADIUS,
-  SCREENHEIGHT,
-  SCREENWIDTH,
-  screenContainer,
-  textlight_regular,
-  textlightfaint_semibold,
-} from "../assets/constants";
 import { BackNavBarWithProfile } from "../components/global/BackNavBarWithProfile";
-import { TransparentContainer } from "../components/productDetails/TransparentContainer";
-import { GradientBox } from "../components/global/GradientBox";
+import { FavouriteItem, Item } from "../components/favourite/Item";
+import { screenContainer } from "../assets/constants";
+
+const testData: FavouriteItem[] = [
+  {
+    id: 1,
+    imageURl:
+      "https://media.istockphoto.com/id/523168994/photo/cappuccino-with-coffee-beans.jpg?s=612x612&w=0&k=20&c=qhRFxaeTppFykANecfXx8B17JSJYNJgW2KExDrUWKCk=",
+    liked: true,
+    name: "Cappuccino",
+    shortDescription: "With milk",
+    ratting: 4.5,
+    numberOfRattings: 100,
+    longDescription:
+      "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top.",
+  },
+  {
+    id: 2,
+    imageURl:
+      "https://media.istockphoto.com/id/523168994/photo/cappuccino-with-coffee-beans.jpg?s=612x612&w=0&k=20&c=qhRFxaeTppFykANecfXx8B17JSJYNJgW2KExDrUWKCk=",
+    liked: false,
+    name: "Espresso",
+    shortDescription: "Strong coffee",
+    ratting: 4.2,
+    numberOfRattings: 80,
+    longDescription:
+      "Espresso is a concentrated coffee brewed by forcing a small amount of nearly boiling water under pressure through finely-ground coffee beans.",
+  },
+  {
+    id: 3,
+    imageURl:
+      "https://media.istockphoto.com/id/523168994/photo/cappuccino-with-coffee-beans.jpg?s=612x612&w=0&k=20&c=qhRFxaeTppFykANecfXx8B17JSJYNJgW2KExDrUWKCk=",
+    liked: true,
+    name: "Latte",
+    shortDescription: "Creamy coffee",
+    ratting: 4.7,
+    numberOfRattings: 120,
+    longDescription:
+      "Latte is a coffee drink made with espresso and steamed milk. The term comes from the Italian caffè e latte, meaning 'coffee and milk'.",
+  },
+];
 
 export const FavouriteScreen = (): JSX.Element => {
+  const renderItem = ({ item }: { item: FavouriteItem }) => {
+    return (
+      <View>
+        <Item item={item} />
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.screenContainer}>
       <View style={styles.navBar}>
         <BackNavBarWithProfile title="Favorites" />
       </View>
       <View style={styles.screenBody}>
-        <View style={styles.cardContainer}>
-          <GradientBox>
-            <Image
-              source={{
-                uri: "https://media.istockphoto.com/id/523168994/photo/cappuccino-with-coffee-beans.jpg?s=612x612&w=0&k=20&c=qhRFxaeTppFykANecfXx8B17JSJYNJgW2KExDrUWKCk=",
-              }}
-              style={styles.image}
-            />
-            <View style={styles.transparentContainer}>
-              <TransparentContainer />
-            </View>
-            <View style={styles.descriptionBox}>
-              <Text style={{ ...textlightfaint_semibold, fontSize: 14 }}>
-                Description
-              </Text>
-              <Text style={{ ...textlight_regular, fontSize: 12 }}>
-                Cappuccino is a latte made with more foam than steamed milk,
-                often with a sprinkle of cocoa powder or cinnamon on top.
-              </Text>
-            </View>
-          </GradientBox>
-        </View>
+        <FlatList
+          data={testData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal={false}
+          contentContainerStyle={styles.flatListContainer}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
-
-      <Text>FavouriteScreen</Text>
     </SafeAreaView>
   );
 };
@@ -59,29 +82,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   screenBody: {
+    flex: 1,
     paddingHorizontal: 30,
   },
-  cardContainer: {
-    height: SCREENHEIGHT * 0.6,
-    borderRadius: BORDERRADIUS.radius_25,
-    overflow: "hidden",
-  },
-  image: {
-    position: "relative",
-    height: SCREENHEIGHT * 0.43,
-    width: SCREENWIDTH - 60,
-  },
-  transparentContainer: {
-    position: "absolute",
-    height: 133,
-    width: SCREENWIDTH - 60,
-    top: SCREENHEIGHT * 0.43 - 133,
-    borderTopRightRadius: BORDERRADIUS.radius_25,
-    borderTopLeftRadius: BORDERRADIUS.radius_25,
-    overflow: "hidden",
-  },
-  descriptionBox: {
-    paddingVertical: 16,
-    paddingHorizontal: 31,
+  flatListContainer: {
+    display: "flex",
+    gap: 28,
+    paddingBottom: 70,
   },
 });
