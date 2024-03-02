131840b38a4d0b41b17e4276f29a1d9760dda1f9
devitoDbug <davidOchiengy@gmail.com>
2024-03-02 15:14:13 +0300

    feat: Implement CRUD operations for category table

---
+++ API/db/query/category.sql
@@ -0,0 +1,35 @@
+-- name: CreateCategory :one
+INSERT INTO "category" (
+    category_name
+) VALUES (
+             $1
+         )
+RETURNING *;
+
+-- name: ListCategory :many
+SELECT * FROM "category"
+WHERE deleted_at IS NULL
+ORDER BY category_id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateCategoryName :one
+UPDATE "category"
+SET category_name = $2, updated_at = now()
+WHERE category_id = $1 AND deleted_at IS NULL
+RETURNING  *;
+
+-- name: DeleteCategoryTemporarily :one
+UPDATE "category"
+SET deleted_at = now()
+WHERE category_id = $1 AND deleted_at IS NULL
+RETURNING  *;
+
+-- name: RestoreCategory :one
+UPDATE "category"
+SET deleted_at = NULL
+WHERE category_id = $1 AND deleted_at IS NOT NULL
+RETURNING  *;
+
+-- name: DeleteCategory :one
+DELETE FROM "category" WHERE category_id = $1 RETURNING *;
