d3aab95bf6f26a404de648343b70dbabad83ec77
devitoDbug <davidOchiengy@gmail.com>
2024-03-05 08:38:30 +0300

    test: Add unit test for CreateUser function

--- API/db/sqlc/user_test.go
+++ API/db/sqlc/user_test.go
@@ -1,1 +1,55 @@
 package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createDummyUser(fName string) User {
+	newUser := User{
+		Fname: sql.NullString{
+			String: fName,
+			Valid:  true,
+		},
+		Sname: sql.NullString{
+			String: fName + "lst",
+			Valid:  true,
+		},
+		Email:    fName + "@gmail.com",
+		Password: uuid.NewString(),
+		PhotoURL: sql.NullString{
+			String: "user1PhotoURl",
+			Valid:  true,
+		},
+	}
+
+	return newUser
+}
+
+func TestQueries_CreateUser(t *testing.T) {
+	newUser := createDummyUser("James")
+	arg := CreateUserParams{
+		Fname:    newUser.Fname,
+		Sname:    newUser.Sname,
+		Email:    newUser.Email,
+		Password: newUser.Password,
+		PhotoURL: newUser.PhotoURL,
+	}
+
+	user1, err := testQueries.CreateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user1)
+
+	require.Equal(t, arg.Fname, user1.Fname)
+	require.Equal(t, arg.Sname, user1.Sname)
+	require.Equal(t, arg.Email, user1.Email)
+	require.Equal(t, arg.Password, user1.Password)
+	require.Equal(t, arg.PhotoURL, user1.PhotoURL)
+	require.NotEmpty(t, user1.CreatedAt)
+	require.NotEmpty(t, user1.UpdatedAt)
+	require.Empty(t, user1.DeletedAt)
+}
--- API/go.mod
+++ API/go.mod
@@ -2,4 +2,14 @@ module coffeeHouse_API
 
 go 1.21.3
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
